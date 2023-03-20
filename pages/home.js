bd916b58560b3f90953a44805f1b86ae6c1481dc
Atikur Rahaman <atikurahad@gmail.com>
2023-03-20 23:48:21 +0600

    card added

--- pages/home.js
+++ pages/home.js
@@ -3,11 +3,21 @@ import Link from "next/link";
 const Home = () => {
   return (
     <div>
-      <h1>I am from home</h1>
-      <Link href={"/profile"}>
-        Profile
-      </Link>
-      <Link href={"/about"}>About</Link>
+      <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1">
+        <section>
+          <div className="h-[22rem] w-[15rem] bg-[#f3f3f3]">
+            <img className="h-[8rem] w-[5rem]]"
+              src="http://silkensewing.com/assets/img/mission-vision/mission.png"
+              alt="vision"
+            />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Asperiores, maxime?
+            </p>
+            <button>Click</button>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
