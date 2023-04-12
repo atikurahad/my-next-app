f962c6c8e769cf44f0c07cbffa61db67203bca3e
Atikur Rahaman <atikurahad@gmail.com>
2023-04-13 03:00:53 +0600

    partner added

--- pages/about.js
+++ pages/about.js
@@ -9,80 +9,31 @@ const About = () => {
         <title>About page</title>
       </Head>
 
-      {/* <div className="flex  justify-center	items-center	py-4">
-        <img src="circle_left.png" alt="" />
-        <span className="text-2xl text-blue-900 font-bold mx-2 ">ABOUT US</span>
-        <img src="circle_right.png" alt="" />
-      </div> */}
+      <div className="">
+        <div className="grid">
 
-      <div className="grid lg:grid-cols-2 sm:grid-cols-1 gap-4 items-center mx-[12rem] my-4">
-        {/* <section className="mx-5">
-          <h1 className="text-3xl text-black  font-bold">
-            SILKEN SEWING - to be one of the most regarded RMG operations in
-            Bangladesh
-          </h1>
-          <p className="text-gray-600">
-            SILKEN SEWING has started its journey in 2008 in a small scale knit
-            apparel manufacturing with the vision of having one of the regarded
-            & trusted home for the Apparel manufacturing in the world. It has
-            been setup by highly educated and well –experienced entrepreneur
-            with excellent track record of Industrial Management. It is managed
-            by highly qualified & experienced personnel. Today, we consider
-            ourselves as one of the rapid growing company of having more than
-            2000 family members with the annual turnover of $25 million It has
-            further diversified & expending its production range towards the
-            Outerwear & Sportswear areas & will start our outerwear unit by
-            2021. Silken has its own design team in UK who continuously provides
-            design & development supports to the customer. Silken Sewing is
-            committed to deliver highest level of service, quality & value to
-            its customer through a responsible business operation which ensures
-            fair treatment to all employees, Community, environments &
-            Stakeholders. We believe that our employee are our greatest asset
-            and invest in their welfare and development during & beyond time
-            they work for us. Silken also has taken some CSR initiatives as a
-            Corporate Social responsibility like Sky`s the Limit, Healthy Child
-            Wealthy Bangladesh.
-          </p>
-          <article className=" flex items-center justify-center">
-            <img
-              className="h-[8rem] w-[8rem]"
-              src="12-years-logo.jpg"
-              alt="logo"
-            />
-            <button className="btn bg-sky-500 btn-wide text-white">
-              Read More
-            </button>
-
-          </article>
-        </section> */}
-
-
-        <section className="my-8">
-          <img src="http://silkensewing.com/assets/img/XsLkbpn_d.webp" alt="" />
-        </section>
+        </div>
       </div>
-
 
       <div className="flex flex-auto justify-center">
         <button className=" text-white bg-black font-bold text-xl  my-10  py-8 px-24 hover:text-yellow-500">
           Our Major Customers
         </button>
       </div>
-      <div className="mt-10">
-        <div className="grid lg:grid-cols-4 gap-4 sm:grid-cols-2 mx-auto items-center mt-5">
-          <section>
-            <img src="ethical.png" alt="" />
+      <div className="py-10">
+        <div className="grid lg:grid-cols-2 gap-4 sm:grid-cols-1 mx-auto justify-items-center mt-5 ">
+          <section className="border-double border-4 border-sky-500">
+            <img src="mk-jumbd.jpg" alt="" />
           </section>
-          <section>
-            <img src="second.jpg" alt="" />
+          <section className="border-double border-4 border-sky-500">
+            <img src="mk-denim.jpg" alt="" />
           </section>
-          <section>
-            <img src="third.png" alt="" />
+          <section className="border-double border-4 border-sky-500">
+            <img src="mk-oo.jpg" alt="" />
           </section>
-          <section>
-            <img src="fourth.png" alt="" />
+          <section className="border-double border-4 border-sky-500">
+            <img src="mk-limonda.jpg" alt="" />
           </section>
-
         </div>
       </div>
 
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,25 +1,35 @@
 const Contact = () => {
   return (
-    <div className="bg-white">
+    <div className="bg-white ">
       <p className="text-center mx-auto">Contact details</p>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 justify-items-center">
-        <section className="text-blue-700 font-bold text-xl">
-          <p className="text-blue-900 text-2xl font-bold">Md. Karimul Hassan</p>
-          <p>Managing Director</p>
-          <p>Cell:+88 01711-974027</p>
-          <p>E-mail: hassan@mkattire.com</p>
+        <section className="text-blue-700 font-bold text-xl h-96 shadow-2xl p-4 my-5">
+          <div className="my-5">
+            <p className="text-blue-900 text-4xl font-bold">
+              Md. Karimul Hassan
+            </p>
+            <p>Managing Director</p>
+            <p>Cell:+88 01711-974027</p>
+            <p>E-mail: hassan@mkattire.com</p>
+          </div>
         </section>
-        <section className="text-blue-700 font-bold text-xl">
-          <p>Md. Mahatab Alam</p>
-          <p> Chairman</p>
-          <p>Cell:+88 01716-670738</p>
-          <p>E-mail: alam@mkattire.com</p>
+        <section className="text-blue-700 font-bold text-xl p-4 my-5 shadow-2xl">
+          <div className="my-5">
+            <p className="text-blue-900 text-4xl font-bold">Md. Mahatab Alam</p>
+            <p> Chairman</p>
+            <p>Cell:+88 01716-670738</p>
+            <p>E-mail: alam@mkattire.com</p>
+          </div>
         </section>
-        <section className="text-blue-700 font-bold text-xl">
-          <p>Md. Karimul Hassan</p>
-          <p>Managing Director</p>
-          <p>Cell:+88 01711-974027</p>
-          <p>E-mail: hassan@mkattire.com</p>
+        <section className="text-blue-700 font-bold text-xl p-4 my-5 shadow-2xl">
+          <div className="my-5">
+            <p className="text-blue-900 text-4xl font-bold">
+              Md. Jobaidul Karim
+            </p>
+            <p> Director</p>
+            <p>Cell:+88 01841-042989</p>
+            <p>E-mail: jkarim@mkattire.com</p>
+          </div>
         </section>
       </div>
     </div>
